a9e886469ab071b0fce13667951af9e7a227c3a1
fuyikun <1281363146@qq.com>
2020-12-24 11:18:37 +0800

    🚧项目搭建

---
+++ remax.config.js
@@ -0,0 +1,12 @@
+const less = require('@remax/plugin-less');
+module.exports = {
+  plugins: [
+    less({
+      lessOptions: {
+        globalVars: {
+          'primary-color': '"#4569d4"',
+        },
+      },
+    }),
+  ],
+};
---
+++ src/app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pages: ['pages/index/index'],
+  window: {
+    navigationBarTitleText: '这是一个标题',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
