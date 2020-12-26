ba860b1a470fa4e867440d1681a3614f835ec887
fuyikun <1281363146@qq.com>
2020-12-27 00:50:34 +0800

    ✨feature(captcha):验证码-静态界面

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pages: ['pages/index/index', 'pages/login/index', 'pages/dinner/index'],
+  pages: ['pages/index/index', 'pages/login/index', 'pages/dinner/index', 'pages/captcha/index'],
   window: {
     navigationBarBackgroundColor: '#2f2f2f'
   }
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
-import { View, Text, Image } from 'remax/wechat';
-import styles from './index.less';
-import { redirectTo } from 'remax/one';
-import { Button } from 'annar';
+import * as React from "react";
+import { View, Text, Image } from "remax/wechat";
+import styles from "./index.less";
+import { redirectTo } from "remax/one";
+import { Button } from "annar";
 
 export default () => {
-
   return (
     <View className={styles.app}>
       <View className={styles.header}>
@@ -15,26 +14,31 @@ export default () => {
           alt="logo"
         />
         <View className={styles.text}>
-          编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
+          编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{" "}
           开始1
-
         </View>
-        <Button onTap={() => {
-          redirectTo({
-            url: '/pages/login/index',
-          })
-        }
-        }>去登录页面</Button>
-        <Button onTap={() => {
-          redirectTo({
-            url: '/pages/dinner/index',
-          })
-        }
-        }>去开台页面</Button>
+        <Button
+          onTap={() => {
+            redirectTo({
+              url: "/pages/login/index",
+            });
+          }}
+        >
+          去登录页面
+        </Button>
+        <Button
+          onTap={() => {
+            redirectTo({
+              url: "/pages/dinner/index",
+            });
+          }}
+        >
+          去开台页面
+        </Button>
       </View>
 
       {redirectTo({
-        url: '/pages/dinner/index',
+        url: "/pages/captcha/index",
       })}
     </View>
   );
