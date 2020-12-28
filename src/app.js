8e671b6409b7f33687e34dcb7381b0b060594fc8
fuyikun <1281363146@qq.com>
2020-12-28 13:59:24 +0800

    ➕feature(app):添加请求工具wechat-request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import './app.less';
 import 'annar/dist/annar.css';
+import { useAppEvent } from 'remax/macro';
+import { queryUpgradePackages } from '@/services/commons';
 
-const App = props => props.children;
+const App = props => {
+  useAppEvent('onLaunch', async () => {
+    const res = await queryUpgradePackages();
+    console.log('res', res)
+  })
+  return props.children
+};
 
 export default App;
