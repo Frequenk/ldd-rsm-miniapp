c1915b571fb290db5c60d9fbd4501fc42d4b0f5d
fuyikun <1281363146@qq.com>
2020-12-30 16:46:06 +0800

    🎨feature(menu):更改购物车组件的存放位置

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -3,7 +3,7 @@ import { View } from "remax/wechat";
 import Tabs from "./components/Tabs";
 import styles from "./index.less";
 import { leftData, rightData } from "./fakeData.js";
-import ShoppingCar from "@/components/ShoppingCar";
+import ShoppingCar from "./components/ShoppingCar";
 
 const Menu = () => {
   return (
