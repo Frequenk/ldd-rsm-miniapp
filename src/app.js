246a501e848080068f65390f964f730095a8755a
fuyikun <1281363146@qq.com>
2021-01-03 13:53:28 +0800

    ✨feature(app):成功与服务器建立socket连接

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import "./app.less";
 import "annar/dist/annar.css";
 import { useAppEvent } from "remax/macro";
 import { queryUpgradePackages } from "@/services/commons";
+import io from '@holytiny/wxmp-socket.io-client';
 
 export const CommonContext = createContext({});
 export const ShoppingCarContext = createContext({});
@@ -10,8 +11,18 @@ export const ShoppingCarContext = createContext({});
 const App = (props) => {
   const [shoppingCarDishes, setShoppingCarDishes] = useState([]);
   useAppEvent("onLaunch", async () => {
-    const res = await queryUpgradePackages();
-    console.log("res", res);
+    // const res = await queryUpgradePackages();
+    // console.log("res", res);
+    var socket = io(`http://localhost:3000?roomid=1`, { transports: ['websocket'] });
+    console.log('socket', socket)
+    socket.on('connect', () => {
+      console.log('连接上了呀呀呀呀')
+    });
+    socket.on('addCart', function (data) {
+      console.log(data);
+    });
+    socket.emit('addCart', 'addCart');
+    console.log('666666666666', 666666666666)
   });
   return (
     <ShoppingCarContext.Provider value={{ shoppingCarDishes, setShoppingCarDishes }}>
