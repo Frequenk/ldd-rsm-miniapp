8943096c2864944766a2c57fe6b8865e1fd2a730
fuyikun <1281363146@qq.com>
2021-01-10 14:19:59 +0800

    🚧feature(app):对接token

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import { useAppEvent } from "remax/macro";
 import { shoppingCarOperate } from '@/core/shoppingcar';
 import axios from 'axios'
 import mpAdapter from 'axios-miniprogram-adapter'
+import { login, setStorageSync, getStorageSync } from "remax/wechat";
+
 axios.defaults.adapter = mpAdapter
 
 export const CommonContext = createContext({});
@@ -14,22 +16,46 @@ export const ShoppingCarContext = createContext({});
 let setShoppingCarDishes;
 
 const App = (props) => {
+  console.log('props', props)
   const [shoppingCarDishes, setDishes] = useState([]);
   const [shoppingCarMsg, setMsg] = useState('');
+  const user = {};
+  user.name = '王小明'
 
 
-  axios.get('http://leiduoduo.free.idcfengye.com/user/getUserList')
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 
   useAppEvent("onLaunch", async () => {
-    setShoppingCarDishes = shoppingCarOperate(1, '王小明', setMsg, setDishes);
+    // 初始化socket及购物车函数
+    setShoppingCarDishes = shoppingCarOperate(1, user?.name, setMsg, setDishes);
 
+    // 初始化请求工具
+    axios.defaults.baseURL = 'http://leiduoduo.free.idcfengye.com';
 
+    // 添加响应拦截器
+    axios.interceptors.response.use(function (response) {
+      const { data: { data } } = response;
+      return data;
+    }, function (error) {
+      return Promise.reject(error);
+    });
+
+    const { code } = await login();
+    const token = await axios.post(`user/userToken?code=${code}`);
+    await setStorageSync('token', token)
+    const tokens = await getStorageSync('token')
+    console.log('getStorage(token)', tokens)
+
+    // 添加请求拦截器
+    axios.interceptors.request.use(function (config) {
+      console.log('config', config)
+      config.headers.Authorization = `Bearer 666`
+      return config;
+    }, function (error) {
+      return Promise.reject(error);
+    });
+
+    const users = await axios.get(`user/token`);
+    console.log('users', users)
   });
 
 
