2382e1547375fae820ccc4132f7570f12de679e1
fuyikun <1281363146@qq.com>
2021-01-13 21:46:33 +0800

    ✨feature(PageLoading):PageLoading添加显示文本功能

--- src/components/PageLoading/index.jsx
+++ src/components/PageLoading/index.jsx
@@ -3,13 +3,14 @@ import { View } from "remax/one";
 import { Loading } from "annar";
 import styles from "./index.less";
 
-const PageLoading = () => {
+const PageLoading = ({ text }) => {
   return (
     <View className={styles.container}>
       <View className={styles.content} />
       <View className={styles.loading}>
         <Loading type="anna" color="#f56330" />
       </View>
+      <View className={styles.text}>{text}</View>
     </View>
   );
 };
